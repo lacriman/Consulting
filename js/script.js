86c6d506bb6a1a51f3b735609351a4a073937cc6
lacriman <proskurin.yarik@gmail.com>
2023-08-27 21:45:31 +0200

    1/2 adapt, 1/2 burger, 1/2 why, 1/2 form, -btn ico

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-const tabs = document.querySelectorAll('.tab');
-const contents = document.querySelectorAll('.content');
+let tabs = document.querySelectorAll('.tab');
+let tabContent = document.querySelectorAll('.content');
+let slider = document.querySelector('.slider');
 
 function showTabContent(index) {
-  tabs.forEach(tab => tab.classList.remove('active'));
-  tabs[index].classList.add('active');
+    tabs.forEach(tab => tab.classList.remove('active'));
+    tabs[index].classList.add('active');
 
-  contents.forEach(content => content.classList.remove('active'));
-  contents[index].classList.add('active');
+    tabContent.forEach(content => content.classList.remove('active'));
+    tabContent[index].classList.add('active');
+
+    // Move the slider to the active tab
+    let activeTab = tabs[index];
+    let sliderOffset = activeTab.offsetTop - activeTab.parentElement.offsetTop;
+    slider.style.transform = `translateY(${sliderOffset}px)`;
 }
 
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+        showTabContent(index);
+    });
+});
+
 showTabContent(0); // Show the first tab content by default
+
 
 // --------------------------------------------------------------------------------------------
 const burgerMenu = document.querySelector('.header__hamburger');
